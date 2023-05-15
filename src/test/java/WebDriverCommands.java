601a6390a0a816bd6ab8ef2aeee6ee608b3fe1ae
Zoran <z.malinovski79@gmail.com>
2023-05-15 14:39:02 +0200

    added type of web driver commands in comment

--- src/test/java/WebDriverCommands.java
+++ src/test/java/WebDriverCommands.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class WebDriverCommands {
+//Browser Commands: (close(); quit();),
+//Conditional Commands: (isDisplayed(); isEnabled(); isSelected(); ),
+//Wait Commands: (Implicitwait(); Explicitwait();),
+//Get Commands:(get(); getTitle(); getPageSource(); getCurrentUrl(); getText();)
+//Navigation Commands: (navigate().forward(); navigate.back(); navigate().to(); navigate().refresh());
+//Switch Commands: (switchTo().frame(); switchTo().alert(); SwitchTo().defaultContent(); switchTo()window(); DriverWindowHandles());
+
+public class WebDriverCommands {
 }
